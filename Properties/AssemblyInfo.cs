a9268f61633f99be9f17dba32d595cef389add43
Neil Nordhaus <neil@nordhaus.us>
2015-03-21 12:20:16 -0400

    Updated Readme with the user stories copied from Guy Royse's Kata Created an empty solution to start with.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Vending_Machine_Kata")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cd9f5aa5-e1f9-4c42-8e50-9c4003416fd9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.*")]
---
+++ VendingMachineTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Vending_Machine_Kata
+{
+    [TestClass]
+    public class VendingMachineTests
+    {
+        public class VendingMachine_Should
+        {
+            [TestMethod]
+            public void Test1()
+            {
+
+            }
+        }
+    }
+}
