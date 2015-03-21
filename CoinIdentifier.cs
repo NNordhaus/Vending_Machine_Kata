57e5f0304413b87ae74a901b9ae4d15836c43b3c
Neil Nordhaus <neil@nordhaus.us>
2015-03-21 15:48:34 -0400

    Added a coin identifier class, can recognize a Nickel by size and weight.

---
+++ CoinIdentifier.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vending_Machine_Kata
+{
+    public class CoinIdentifier
+    {
+        public int GetCoinValue(Coin c)
+        {
+            return 5;
+        }
+    }
+
+    public class Coin
+    {
+        public int Value { get; set;}
+        public float WeightInGrams { get; set;}
+        public float DiameterInMM { get; set;}
+        public float ThicknessInMM { get; set; }
+    }
+}
---
+++ CoinIdentifierTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Vending_Machine_Kata
+{
+    [TestClass]
+    public class CoinIdentifierTests
+    {
+        [TestClass]
+        public class GetCoinValue_Should
+        {
+            // Using coin specs found here: http://www.usmint.gov/about_the_mint/index.cfm?flash=yes&action=coin_specifications 
+
+            [TestMethod]
+            public void Match_5_cents_by_a_Nickels_Size_and_weight()
+            {
+                var sut = new CoinIdentifier();
+
+                var actual = sut.GetCoinValue(new Coin() { WeightInGrams = 5f, DiameterInMM = 21.21f, ThicknessInMM = 1.95f });
+
+                Assert.AreEqual(5, actual);
+            }
+        }
+    }
+}
